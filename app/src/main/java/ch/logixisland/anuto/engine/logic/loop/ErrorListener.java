32d9c9b406e33c94b11b5df8a774a8ceb2bbf10a
Metro Lap <40216165+lap-does-things@users.noreply.github.com>
2024-04-12 00:05:30 +0300

    Update ErrorListener.java

--- app/src/main/java/ch/logixisland/anuto/engine/logic/loop/ErrorListener.java
+++ app/src/main/java/ch/logixisland/anuto/engine/logic/loop/ErrorListener.java
@@ -1,5 +1,5 @@
 package ch.logixisland.anuto.engine.logic.loop;
-
+// по сути это не нужно, если ты не кривоносый чурбан, который будет вызывать ошибки
 public interface ErrorListener {
     void error(Exception e, int loopCount);
 }
