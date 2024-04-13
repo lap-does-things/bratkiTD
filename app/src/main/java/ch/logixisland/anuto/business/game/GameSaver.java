bd6e6e246e287e9bbe521a523aa5e3c1f4b63d19
Лэп <40216165+lap-does-things@users.noreply.github.com>
2024-04-13 10:41:41 +0300

    Update GameSaver.java

--- app/src/main/java/ch/logixisland/anuto/business/game/GameSaver.java
+++ app/src/main/java/ch/logixisland/anuto/business/game/GameSaver.java
@@ -10,11 +10,11 @@ import ch.logixisland.anuto.engine.logic.GameEngine;
 import ch.logixisland.anuto.engine.logic.persistence.GamePersister;
 import ch.logixisland.anuto.engine.render.Renderer;
 import ch.logixisland.anuto.util.container.KeyValueStore;
-
+// сохраняльщик
 public class GameSaver {
 
     private static final String TAG = GameSaver.class.getSimpleName();
-
+ //пулим всю инфу об игре для сейва
     private final GameEngine mGameEngine;
     private final GameLoader mGameLoader;
     private final GamePersister mGamePersister;
@@ -22,7 +22,7 @@ public class GameSaver {
     private final WaveManager mWaveManager;
     private final ScoreBoard mScoreBoard;
     private final SaveGameRepository mSaveGameRepository;
-
+// вставляем её 
     public GameSaver(GameEngine gameEngine, GameLoader gameLoader, GamePersister gamePersister,
                      Renderer renderer, WaveManager waveManager, ScoreBoard scoreBoard, SaveGameRepository saveGameRepository) {
         mGameEngine = gameEngine;
@@ -33,7 +33,7 @@ public class GameSaver {
         mScoreBoard = scoreBoard;
         mSaveGameRepository = saveGameRepository;
     }
-
+// автосейв когда игра сворачивается и т. д. 
     public void autoSaveGame() {
         if (mGameEngine.isThreadRunning() && mGameEngine.isThreadChangeNeeded()) {
             mGameEngine.post(this::autoSaveGame);
@@ -47,7 +47,7 @@ public class GameSaver {
         if (mGameEngine.isThreadRunning() && mGameEngine.isThreadChangeNeeded()) {
             throw new RuntimeException("This method cannot be used when the game thread is running!");
         }
-
+// всё то же для сохранения
         SaveGameInfo saveGameInfo = mSaveGameRepository.createSaveGame(
                 mRenderer.getScreenshot(),
                 mScoreBoard.getScore(),
@@ -58,7 +58,7 @@ public class GameSaver {
         saveGameState(mSaveGameRepository.getGameStateFile(saveGameInfo));
         return saveGameInfo;
     }
-
+//ручной сейв игры. не имплементирован
     void saveGameState(File stateFile) {
         Log.i(TAG, "Saving game...");
         KeyValueStore gameState = new KeyValueStore();
